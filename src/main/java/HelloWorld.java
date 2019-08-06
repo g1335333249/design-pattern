334459c7a749d45bdd4b5b2536cef9d18f1def21
关鹏 <guanpeng@hffss.com>
2019-08-06 15:40:36 +0800

    init

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+/**
+ * @author guanpeng
+ * @date 2019-08-06 15:39
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
